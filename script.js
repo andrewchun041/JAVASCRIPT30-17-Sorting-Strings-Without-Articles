2b66e7113e8abd8410714187accb3cd1aaef302c
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-07-26 16:37:16 -0700

    map each sortedBand into <li> tag and add it into bands list as html

--- script.js
+++ script.js
@@ -30,4 +30,8 @@ const sortedBands = bands.sort(
     // (a, b) => strip(a) > strip(b) ? 1 : -1
 );
 
+document.querySelector('#bands').innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('');
+
 console.log(sortedBands);
+console.log(sortedBands.map(band => `<li>${band}</li>`));
+console.log(sortedBands.map(band => `<li>${band}</li>`).join(''));
